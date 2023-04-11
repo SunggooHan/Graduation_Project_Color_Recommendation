db2dee5c133c6556fd21b4cede2b605ec93e257a
SunggooHan <77471327+SunggooHan@users.noreply.github.com>
2023-04-11 17:10:00 +0900

    Add files via upload

--- color_recommendation.py
+++ color_recommendation.py
@@ -8,7 +8,7 @@ import sys
 from  PIL  import Image
 import colorsys
 
-img_origin = cv2.imread('img.png', cv2.IMREAD_UNCHANGED)
+img_origin = cv2.imread('example2.png', cv2.IMREAD_UNCHANGED)
 original = img_origin.copy()
 
 l = int(max(5, 6))
@@ -80,9 +80,9 @@ centroid=kmeans.cluster_centers_
 
 percent=[]
 for i in range(len(centroid)):
-    j=labels.count(i)
-    j=j/(len(labels))
-    percent.append(j)
+  j=labels.count(i)
+  j=j/(len(labels))
+  percent.append(j)
 
 # plot a pie chart
 plt.pie(percent,colors=np.array(centroid/255),labels=np.arange(len(centroid)))
@@ -120,8 +120,8 @@ hexlabels = []
 
 #get the main color
 for i in range(clt.cluster_centers_.shape[0]):
-    colors.append(tuple(clt.cluster_centers_[i]/255))
-    hexlabels.append(cs.to_hex(tuple(clt.cluster_centers_[i]/255)))
+  colors.append(tuple(clt.cluster_centers_[i]/255))
+  hexlabels.append(cs.to_hex(tuple(clt.cluster_centers_[i]/255)))
 
 #create pie chart for color
 plt.pie(hist,labels=hexlabels,colors=colors,autopct='%1.1f%%')
@@ -211,26 +211,153 @@ final_hsvlabels = kmeans_color_to_hsv(img)
 original_hsv = final_hsvlabels[0]
 print(original_hsv)
 
-#논문에 나온 hsv distance 계산 방식
-hsv = original_hsv
+#정규화
+df = pd.read_csv('ColorDataset.csv')
+hsva_h = df["hsva/h"]
+hsva_s = df["hsva/s"]
+hsva_v = df["hsva/v"]
 
+#h값 [0,2pi] 정규화
 df = pd.read_csv('ColorDataset.csv')
 hsva_h = df["hsva/h"].to_numpy()
-hsva_s = df["hsva/s"].to_numpy()
-hsva_v = df["hsva/v"].to_numpy()
+hsva_h = hsva_h.reshape(-1,1)
+
+from sklearn.preprocessing import MinMaxScaler
+from math import pi
+
+scaler = MinMaxScaler(feature_range=(0,2*pi))
+scaler.fit(hsva_h)
+hsva_h_scaled = scaler.transform(hsva_h)
+
+hsva_h = pd.DataFrame(hsva_h_scaled, columns=['hsva_h'])
+
+#sv값 [0,1] 정규화
+from sklearn.preprocessing import minmax_scale
+
+hsva_s = pd.DataFrame(data=minmax_scale(hsva_s))
+hsva_v = pd.DataFrame(data=minmax_scale(hsva_v))
+
+hsva_s.columns = ["hsva_s"]
+hsva_v.columns = ["hsva_v"]
+
+scaled_hsv_set = hsva_h.join(hsva_s).join(hsva_v)
+
+#(sin(h1)*s1*v1 - sin(h2)*s2*v2 )^2 + (cos(h1)*s1*v1 - cos(h2)*s2*v2)^2 + ( v1 - v2 )^2
+
+hsv = original_hsv
+hsv_h = hsv[0]
+hsv_s = hsv[1]
+hsv_v = hsv[2]
+
+#추출한 h 값 정규화
+hsv_h = (hsv_h/360)*2*pi
+
+#추출한 s값 정규화
+hsv_s = hsv_s/100
+
+#추출한 s값 정규화
+hsv_v = hsv_v/100
+
+scaled_hsv = [hsv_h, hsv_s, hsv_v]
+
+#거리계산
+import math
+
+hsv_h = scaled_hsv[0]
+hsv_s = scaled_hsv[1]
+hsv_v = scaled_hsv[2]
+
+df = scaled_hsv_set
+df_origin = pd.read_csv('ColorDataset.csv')
+hsva_h = df["hsva_h"].to_numpy()
+hsva_s = df["hsva_s"].to_numpy()
+hsva_v = df["hsva_v"].to_numpy()
 dist = []
 
 for i in range(len(hsva_h)):
-    if (hsv[0]-hsva_h[i]) < 180:
-        h_diff = hsv[0]-hsva_h[i]
-    else:
-        h_diff = 360 - (hsv[0]-hsva_h)
-
-    distance = (hsv[1]*abs(h_diff) + abs(hsv[1]-hsva_s[i]) + abs(hsv[2]-hsva_v[i]))
+    distance = ((math.sin(hsv_h)*hsv_s*hsv_v)-(math.sin(hsva_h[i])*hsva_s[i]*hsva_v[i]))*((math.sin(hsv_h)*hsv_s*hsv_v)-(math.sin(hsva_h[i])*hsva_s[i]*hsva_v[i]))
+    + ((math.cos(hsv_h)*hsv_s*hsv_v)-(math.cos(hsva_h[i])*hsva_s[i]*hsva_v[i]))*((math.cos(hsv_h)*hsv_s*hsv_v)-(math.cos(hsva_h[i])*hsva_s[i]*hsva_v[i]))
+    + (hsv_v-hsva_v[i])*(hsv_v-hsva_v[i])
+    
     dist.append(distance)
 
 min(dist)
-
 num = dist.index(min(dist))
-df.iloc[num]
-print(df.loc[df['key'] == df.loc[num, 'key']])
+
+print(df_origin.loc[df_origin['key'] == df_origin.loc[num, 'key']])
+
+##############################################################################################
+#Find corners of the furniture
+gray = cv2.cvtColor(img_origin, cv2.COLOR_BGR2GRAY)
+
+#Apply Canny edge detection
+edges = cv2.Canny(gray, 50, 150)
+
+# Find contours
+contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+# Loop through each contour and find the corners
+for cnt in contours:
+    # Find the perimeter of the contour
+    perimeter = cv2.arcLength(cnt, True)
+    
+    # Approximate the contour to a polygon
+    approx = cv2.approxPolyDP(cnt, 0.02 * perimeter, True)
+    
+    # If the polygon has four corners, draw a rectangle around it
+    if len(approx) == 4:
+        cv2.rectangle(img, (approx[0][0][0], approx[0][0][1]), (approx[2][0][0], approx[2][0][1]), (0, 255, 0), 2)
+
+# Print the number of corners detected
+print("Number of corners detected(Canny):", len(approx))
+
+# Display the image with the rectangular contours
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.show()
+
+##############################################################################################
+# Shi-Tomasi Detection
+
+# Define the parameters for the Shi-Tomasi corner detector
+max_corners = 100
+quality_level = 0.01
+min_distance = 10
+
+# Find corners using the Shi-Tomasi corner detector
+corners = cv2.goodFeaturesToTrack(gray, max_corners, quality_level, min_distance)
+
+# Convert the corners to integers
+corners = np.int0(corners)
+
+# Loop through each corner and draw a circle around it
+for corner in corners:
+    x, y = corner.ravel()
+    cv2.circle(img, (x, y), 5, (0, 255, 0), -1)
+
+# Print the number of corners detected
+print("Number of corners detected(Shi-Tomasi):", len(corners))
+
+# Display the image with the corners
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.show()
+
+##############################################################################################
+#Harris Corner Detection
+
+# Find corners using the Harris corner detector
+dst = cv2.cornerHarris(gray, 2, 3, 0.04)
+
+# Threshold the corner response
+dst_thresh = 0.01 * dst.max()
+corner_idxs = np.where(dst > dst_thresh)
+
+# Draw circles around the corners
+for idx in zip(*corner_idxs[::-1]):
+    cv2.circle(img, idx, 5, (0, 255, 0), -1)
+
+# Print the number of corners detected
+print("Number of corners detected(Harris Corner):", len(corner_idxs[0]))
+
+# Display the image with the corners
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.show()
